eea01cfaac5bb306369a80863f691abaaf557e0d
kikuk24 <tirta.afandi24@gmail.com>
2025-04-14 00:34:30 +0700

    feat:add new feature generate code barang

--- barang/simpan.php
+++ barang/simpan.php
@@ -4,13 +4,24 @@
 include '../config/koneksi.php';
 
 $nama = $_POST['nama'];
-$kode = $_POST['kode'];
-$kategori = $_POST['kategori'];
+$kondisi = $_POST['kondisi'];
 $jumlah = $_POST['jumlah'];
 $ruangan_id = $_POST['ruangan_id'];
 
-$query = "INSERT INTO barang (nama_barang, kategori, jumlah, ruangan_id)
-            VALUES ('$nama',  '$kategori', '$jumlah', '$ruangan_id')";
+function generateKodeBarang($conn)
+{
+    $query = "SELECT MAX(id_barang) as max_id FROM barang";
+    $result = mysqli_query($conn, $query);
+    $data = mysqli_fetch_assoc($result);
+
+    $maxId = isset($data['max_id']) ? (int)$data['max_id'] + 1 : 1;
+    return "BRG" . str_pad($maxId, 4, '0', STR_PAD_LEFT);
+}
+
+$kode_barang = generateKodeBarang($conn);
+
+$query = "INSERT INTO barang (nama_barang, kondisi, kode_barang, jumlah, id_ruangan)
+            VALUES ('$nama',  '$kondisi', '$kode_barang', '$jumlah', '$ruangan_id')";
 
 mysqli_query($conn, $query);
 
